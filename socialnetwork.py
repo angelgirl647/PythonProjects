94eb630071f0e5d5b59a994b656584a16841b308
Yuki Fang <yfang4268@bths.edu>
2017-08-11 11:57:17 -0400

    Update socialnetwork.py

--- socialnetwork.py
+++ socialnetwork.py
@@ -5,7 +5,7 @@ class User:
         self.lastname=last
         self.username = username
         self.age=age
-        self.friends=[]
+        self.friends=["Masfi"]
 
     def addFriend(self,friend):
         self.friends.append(friend)
@@ -22,32 +22,41 @@ class User:
     def getConnections(self):
         return self.friends
 
+    def printUser(self):
+        print (self.getFullName())
+        print (self.getUserName())
+        print (self.getAge())
 # entire network i.e all users
 class Network:
 
     # Define the fields and methods for your object here.
     def __init__(self):
-        self.users=[]
+        self.users=["Masfi", "Yuki", "Lena", "Leyla", "Nick"]
 
     def checkUsername(self, username):
-        taken=True
-        while taken:
-            if username in self.users:
-                print("that username is already taken. Choose another one")
-            else:
-                self.users.append(username)
-                taken=False
+        if username in self.users:
+            print("that username is already taken. Choose another one")
+            self.makeAcc()
+        else:
+            self.users.append(username)
+            taken=False
+
+    def getUsersList(self):
+        return self.users
 
     def getTotalUsers(self):
-        return len(self.users)
+        print (len(self.users))
+        print (self.users)
 
-    def checkUsersExist(self,username):
-        exist=False
-        if username in self.users:
-            exist=True
-        else:
-            print("This user does not exist. Add someone else")
-            return False
+    # def checkUsersExist(self,username):
+    #     exist=False
+    #
+    #     if username in self.users:
+    #
+    #
+    #     else:
+    #         print("This user does not exist. Add someone else")
+
 
     def addConnections(self,user2):
         for i in range(len(self.friends)):
@@ -59,38 +68,32 @@ class Network:
                 user2.friends.append(self)
                 print("You are now friends")
 
-
     def makeAcc(self):
-        y=Network()
         first=input("first name:")
         last=input("last name:")
         username=input("username:")
-        y.checkUsername(username)
+        self.checkUsername(username)
         age=input("age:")
-        y.profileInfo()
 
-    def profileInfo(self):
-        x= User(first,last,username,age)
-        print("Profile:")
-        print(x.getFullName())
-        print(x.getUserName())
-        print(x.getAge())
 
 def mainPage():
     y=Network()
+    x=User("Yuki", "Fang", "beep", "17")
     action=input("What do you want to do today? (a) add a new user, (b) add friends, (c)view profile, (d)see total users, (e)exit")
     if action == "a":
         y.makeAcc()
+        print(y.getUsersList())
         mainPage()
     elif action=="b":
         friend=input("who?")
-        y.addConnections(friend)
+        x.addFriend(friend)
+        print(x.getConnections())
         mainPage()
     elif action=="c":
-        y.profileInfo()
+        x.printUser()
         mainPage()
     elif action=="d":
-        y.getTotalUsers
+        y.getTotalUsers()
     else:
         print("BYE")
 
